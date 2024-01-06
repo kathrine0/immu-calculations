c40356d0cd2445e83f74765ce4982a3ecf6c7d33
Kasia Biernat <kasia.biernat@hyland.com>
2024-01-06 17:24:23 +0100

    Chips

--- src/app/components/immu-form/immu-form.component.ts
+++ src/app/components/immu-form/immu-form.component.ts
@@ -5,11 +5,19 @@ import {
   EventEmitter,
   Output,
 } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import {
+  MatChipEditedEvent,
+  MatChipInputEvent,
+  MatChipsModule,
+} from '@angular/material/chips';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+
+import { COMMA, ENTER, SPACE } from '@angular/cdk/keycodes';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-immu-form',
@@ -21,6 +29,9 @@ import { MatCardModule } from '@angular/material/card';
     MatInputModule,
     MatButtonModule,
     MatCardModule,
+    MatChipsModule,
+    MatInputModule,
+    MatIconModule,
   ],
   templateUrl: 'immu-form.component.html',
   styleUrl: './immu-form.component.scss',
@@ -29,17 +40,48 @@ import { MatCardModule } from '@angular/material/card';
 export class ImmuFormComponent {
   @Output() immuWsProvided = new EventEmitter<string[]>();
 
-  immuWs = new FormControl('');
-  immuForm = new FormGroup({
-    immuWs: this.immuWs,
-  });
+  immuWs: Array<{ name: string }> = [];
 
-  onSubmit() {
-    const roughInput = this.immuWs.value;
+  addOnBlur = true;
+  readonly separatorKeysCodes = [COMMA, SPACE, ENTER] as const;
 
-    if (roughInput) {
-      const input = roughInput.trim().split(' ');
-      this.immuWsProvided.emit(input);
+  add(event: MatChipInputEvent): void {
+    const value = (event.value || '').trim();
+
+    if (value) {
+      this.immuWs.push({ name: value });
     }
+
+    event.chipInput!.clear();
+    this.submit();
+  }
+
+  remove(w: { name: string }): void {
+    const index = this.immuWs.indexOf(w);
+
+    if (index >= 0) {
+      this.immuWs.splice(index, 1);
+      this.submit();
+    }
+  }
+
+  edit(w: { name: string }, event: MatChipEditedEvent) {
+    const value = event.value.trim();
+
+    if (!value) {
+      this.remove(w);
+      return;
+    }
+
+    const index = this.immuWs.indexOf(w);
+    if (index >= 0) {
+      this.immuWs[index].name = value;
+      this.submit();
+    }
+  }
+
+  submit() {
+    const input = this.immuWs.map((w) => w.name);
+    this.immuWsProvided.emit(input);
   }
 }
